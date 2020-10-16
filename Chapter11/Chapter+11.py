87c02bf1ae06a0632268be54aacb45b072229ebf
usama998236 <62753842+usama998236@users.noreply.github.com>
2020-10-16 17:39:17 +0500

    Add files via upload

---
+++ Chapter11/Chapter+11.py
@@ -0,0 +1,100 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import itertools
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+
+# In[2]:
+
+
+# Import dataset
+df=pd.read_csv('train.csv')
+
+# Get the shape
+df.shape
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+# Change the labels
+df.loc[(df['label'] == 1) , ['label']] = 'FAKE'
+df.loc[(df['label'] == 0) , ['label']] = 'REAL'
+
+
+# In[10]:
+
+
+df.head()
+df = df.head(3000)
+
+
+# In[11]:
+
+
+# Isolate the labels
+labels = df.label
+labels.head()
+
+
+# In[12]:
+
+
+#Split the dataset
+x_train,x_test,y_train,y_test=train_test_split(df['text'].values.astype('str'), labels, test_size=0.2, random_state=7)
+
+
+# In[13]:
+
+
+#Initialize a TfidfVectorizer
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+
+
+# In[14]:
+
+
+# Fit & transform train set, transform test set
+tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+
+# In[15]:
+
+
+# Initialize the PassiveAggressiveClassifier and fit training sets
+pa_classifier=PassiveAggressiveClassifier(max_iter=50)
+pa_classifier.fit(tfidf_train,y_train)
+
+
+# In[16]:
+
+
+# Predict and calculate accuracy
+y_pred=pa_classifier.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+
+
+# In[17]:
+
+
+# Build confusion matrix
+confusion_matrix(y_test,y_pred, labels=['FAKE','REAL'])
+
